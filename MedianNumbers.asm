a229f10e068dbba2aadb35851e35459bdc67bba2
NickLavoie1 <35705872+NickLavoie1@users.noreply.github.com>
2019-02-04 17:51:33 -0800

    Add files via upload
    second

--- MedianNumbers.asm
+++ MedianNumbers.asm
@@ -7,9 +7,9 @@
 
 	# TODO: Complete these declarations / initializations
 
-	.asciiz "Enter the next number:\n"
+	prompt: .asciiz "Enter the next number:\n"
 
-	.asciiz "Median: "
+	result: .asciiz "Median: "
 
 
 #Text Area (i.e. instructions)
@@ -17,7 +17,78 @@
 
 main:
 
-	# TODO: Write your code here
+	#Prints prompt and takes user input three times. Stores all three inputs in first three registers.
+	li $v0, 4
+	la $a0, prompt
+	syscall
+
+	li $v0, 5
+	syscall 
+	move $t0, $v0
+
+	li $v0, 4
+	la $a0, prompt
+	syscall
+
+	li $v0, 5
+	syscall 
+	move $t1, $v0
+
+	li $v0, 4
+	la $a0, prompt
+	syscall
+
+	li $v0, 5
+	syscall 
+	move $t2, $v0
+
+	li $v0, 4
+	la $a0, result
+	syscall
+
+	#First compare first input with second, start branching from there
+	slt $t3, $t0, $t1
+	bne $t3, $zero, compare_lf_one
+
+	slt $t3, $t1, $t2
+	bne $t3, $zero, compare_lf_two
+
+	j print_second
+
+compare_lf_one:
+	slt $t3, $t2, $t0
+	bne $t3, $zero, print_first
+	j compare_sl
+
+compare_sl:
+	slt $t3, $t1, $t2
+	bne $t3, $zero, print_second
+	j print_third
+
+compare_lf_two:
+	slt $t3, $t2, $t0
+	bne $t3, $zero, print_third
+	j print_first
+
+print_first:
+	li $v0, 1
+	move $a0, $t0
+	syscall
+	j exit
+
+print_second:
+	li $v0, 1
+	move $a0, $t1
+	syscall
+	j exit
+
+print_third:
+	li $v0, 1
+	move $a0, $t2
+	syscall
+
+
+
 
 
 
--- factorial.asm
+++ factorial.asm
@@ -26,6 +26,44 @@ newline:
 main:
 
 	# TODO: Write your code here
+	li $v0, 4
+	la $a0, prompt
+	syscall
+
+	li $v0, 5
+	syscall 
+	move $t0, $v0
+
+	li $t1, 1
+	li $t2, 1
+
+loop:
+	beq $t0, $zero, loop_exit
+	mult $t0, $t1
+	mflo $t1
+	
+	#Print result and newline character
+	li $v0, 1
+	move $a0, $t1
+	syscall
+
+	li $v0, 4
+	la $a0, newline
+	syscall
+
+	sub $t0, $t0, $t2
+	
+	j loop
+
+loop_exit:
+	li $v0, 1
+	move $a0, $t1
+	syscall
+
+	li $v0, 4
+	la $a0, newline
+	syscall
+
 
 exit:
 
